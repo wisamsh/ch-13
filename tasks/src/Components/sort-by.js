aa45e0f3854cf57c9bf66fb0340c601c3a091bac
Wisam Shomar <wisamsh@gmail.com>
2023-07-09 17:37:11 +0300

    Update sort-by.js

--- tasks/src/Components/sort-by.js
+++ tasks/src/Components/sort-by.js
@@ -1,80 +1,62 @@
-import React, { useState, useEffect, useContext  } from 'react';
-import TaskShow from './taskShow';
-import useTasksContext from '../Hooks/use-task-conext';
-import { MutatingDots } from 'react-loader-spinner';
-import Box from '@mui/material/Box';
+import React, { useState, useEffect, useContext } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import TaskContext from "../Context/tasks";
 function TaskLisSortByt() {
-    const { sortTasksById, sortTasksByTitle, sortTasksByStatus, sortTasksByDate, sortTasksByDescription, toggleSortOrder  } = useContext(TaskContext);
-const [sortBy , setsortBy] = useState('task_ID'); 
+    const { sortTasksById, sortTasksByTitle, sortTasksByStatus, sortTasksByDate, sortTasksByDescription, toggleSortOrder } = useContext(TaskContext);
+    const [sortBy, setsortBy] = useState('task_ID');
 
 
-useEffect(() => {
-    //console.log(Completed);
-  }, [sortBy]);
+    useEffect(() => {
 
-    const handleSortChange = (e)=> {
-setsortBy(e.target.value);
-switch (e.target.value) {
-    case 'task_ID':
-        sortTasksById();
-      break;
-    case 'task_title':
-        sortTasksByTitle();
-      break;
-    case 'task_status':
-        sortTasksByStatus();
-      break;
-      case 'task_date':
-        sortTasksByDate();
-      break;
-      case 'task_description':
-        sortTasksByDescription();
-      break;
+    }, [sortBy]);
 
+    const handleSortChange = (e) => {
 
-    default:
-        sortTasksById();
-      break;
-  }
-
-
-   // console.log(useTasksContext);
-   //sortTasksById();
-  // sortTasksByTitle();
-  //sortTasksByStatus();
-  //sortTasksByDate();
-  //sortTasksByDescription();
- // toggleSortOrder();
-}
+        setsortBy(e.target.value);
+        switch (e.target.value) {
+            case 'task_ID':
+                sortTasksById();
+                break;
+            case 'task_title':
+                sortTasksByTitle();
+                break;
+            case 'task_status':
+                sortTasksByStatus();
+                break;
+            case 'task_date':
+                sortTasksByDate();
+                break;
+            case 'task_description':
+                sortTasksByDescription();
+                break;
+            default:
+                sortTasksById();
+                break;
+        }
+    }
 
 
     return (
-    <>
-     <FormControl fullWidth>
-            <InputLabel id="sort_by_select-label">Sort By</InputLabel>
-            <Select
-              labelId="sort_by_select-label"
-              id="sort_by_select"
-              value={sortBy}
-              label="Filter"
-              onChange={handleSortChange}
-            >
-              <MenuItem value={'task_ID'}>ID</MenuItem>
-              <MenuItem value={'task_title'}>Title</MenuItem>
-              <MenuItem value={'task_status'}>Status</MenuItem>
-              <MenuItem value={'task_date'}>Date</MenuItem>
-              <MenuItem value={'task_description'}>Description</MenuItem>
-              
-              
-
-            </Select>
-          </FormControl>
-    </>
+        <>
+            <FormControl fullWidth>
+                <InputLabel id="sort_by_select-label">Sort By</InputLabel>
+                <Select
+                    labelId="sort_by_select-label"
+                    id="sort_by_select"
+                    value={sortBy}
+                    label="Filter"
+                    onChange={handleSortChange}>
+                    <MenuItem value={'task_ID'}>ID</MenuItem>
+                    <MenuItem value={'task_title'}>Title</MenuItem>
+                    <MenuItem value={'task_status'}>Status</MenuItem>
+                    <MenuItem value={'task_date'}>Date</MenuItem>
+                    <MenuItem value={'task_description'}>Description</MenuItem>
+                </Select>
+            </FormControl>
+        </>
 
 
     );
